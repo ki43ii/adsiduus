c74ac02952cf946ac93d69124085e54a2189e998
fredcaine <fefkwt@gmail.com>
2025-03-21 01:42:00 +0300

    added enemy class with super basic functionality

--- playerclass.py
+++ playerclass.py
@@ -1,5 +1,18 @@
+from random import randint
 
 stats = {"barbarian" : (50, 50), "tank" : (20, 80), "healer" : (30, 30), "warrior" : (80, 20)}
+
+class Enemy:
+
+    def __init__(self, enemytype):
+
+        self.attack_strength = randint(10, 100)
+        self.defense = randint(10, 100)
+        self.health = randint(75, 175)
+
+    def identify_type(self):
+
+        pass
 
 class Player:
 
@@ -7,3 +20,8 @@ class Player:
 
         self.attack_strength = stats.get(playertype)[0]
         self.defense = stats.get(playertype)[1]
+        self.health = 200
+
+    def attack(self, enemy):
+        
+        pass
