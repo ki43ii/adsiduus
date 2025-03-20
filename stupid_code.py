648636aa158474ac64abd22e5627599ab380a4f9
ki43ii <atrpwq@proton.me>
2025-03-20 19:53:38 +0300

    modified:   stupid_code.py again again

--- stupid_code.py
+++ stupid_code.py
@@ -1,8 +1,3 @@
 print("fredcaine is weird")
 for i in range(6):
-<<<<<<< HEAD
-    print("ki43ii has no balslslalsl")
-
-=======
-	print("fredcaine has no balaslla")
->>>>>>> 8fc01c2 (modified:   stupid_code.py)
+	print("fredcaine has no balasl")
