ea245fc7588dbfafd4e58355791bce41f3185b12
ki43ii <atrpwq@proton.me>
2025-03-20 19:48:31 +0300

    modified:   stupid_code.py
    modified:   stupid_code.py again

--- stupid_code.py
+++ stupid_code.py
@@ -1,4 +1,8 @@
 print("fredcaine is weird")
 for i in range(6):
+<<<<<<< HEAD
     print("ki43ii has no balslslalsl")
 
+=======
+	print("fredcaine has no balaslla")
+>>>>>>> 8fc01c2 (modified:   stupid_code.py)
