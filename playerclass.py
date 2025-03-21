c1691db49dc3ae411982bc9e6c7cdb24959f887a
fredcaine <fefkwt@gmail.com>
2025-03-21 20:56:03 +0300

    added weapons lol

--- playerclass.py
+++ playerclass.py
@@ -1,16 +1,18 @@
 from random import randint
 
-#(Attack, Defense)
-stats = {"barbarian" : (50, 50), "tank" : (20, 80), "healer" : (30, 30), "warrior" : (80, 20)}
+# (Attack, Defense)
+stats = {"barbarian" : (50, 50, 200, "rip"), "tank" : (20, 80, 400, "invincible"),
+         "healer" : (30, 30, 300, "revive"), "warrior" : (80, 20, 150, "enrage")}
+
+weapons = {"barbarian" : ("short-range knife", "hatchet", "rope", "mace",
+                          "axe", "whip", "throwable axe", "scythe", "javelin"),
+
+           "tank" : ("hands", "bow and arrow", "slab of iron", "crossbow", "shield",
+                     "spiked shield", "titanium knuckles", "pistol", "a literal sniper rifle (lol)")}
 
 enemytypes = {10: "dog", 20: "wolf", 30: "guard", 40: "armed guard", 50: "lead guard",
               60: "wizard", 70: "enraged wizard", 80: "high wizard", 90: "grand sorcerer",
               100: "tarrasque"}
-
-barbarian = stats["barbarian"]
-tank = stats["tank"]
-healer = stats["healer"]
-warrior = stats["warrior"]
 
 class Enemy:
 
@@ -18,26 +20,26 @@ class Enemy:
 
         if enemypower == 0:
             
-            self.attack_strength = randint(10, barbarian[0])
-            self.defense = randint(10, barbarian[1])
+            self.attack_strength = randint(10, 50)
+            self.defense = randint(10, 50)
             self.health = randint(40, 120)
 
         if enemypower == 1:
 
-            self.attack_strength = randint(25, tank[0])
-            self.defense = randint(25, tank[1])
+            self.attack_strength = randint(25, 65)
+            self.defense = randint(25, 65)
             self.health = randint(60, 180)
 
         elif enemypower == 2:
             
-            self.attack_strength = randint(40, healer[0])
-            self.defense = randint(40, healer[1])
+            self.attack_strength = randint(40, 80])
+            self.defense = randint(40, 80)
             self.health = randint(120, 250)
 
         else:
 
-            self.attack_strength = randint(60, warrior[0])
-            self.defense = randint(60, warrior[1])
+            self.attack_strength = randint(60, 120])
+            self.defense = randint(60, 120)
             self.health = randint(150, 300)
 
     def identify_type(self):  # this method identifies the enemy's type.
@@ -53,9 +55,12 @@ class Player:
 
     def __init__(self, playertype):
 
-        self.attack_strength = stats.get(playertype)[0]
-        self.defense = stats.get(playertype)[1]
-        self.health = 200
+        self.level = 1
+        # dividing by 5 to avoid overpowering level 9 players
+        self.attack_strength = stats.get(playertype)[0] * self.level // 5  
+        self.defense = stats.get(playertype)[1] * self.level // 5
+        self.health = stats.get(playertype)[2] * self.level // 5
+        self.special_move = stats.get(playertype)[3] * self.level // 5
 
     def attack(self, enemy):
         
