e0c4f3afb88530c74e84d35179e1fa4938b50495
fredcaine <fefkwt@gmail.com>
2025-03-22 23:43:05 +0300

    COLOURED_ASCII_IMAGE_CREATION has been moved to sprites.py
    function defined in sprites.py to be used in the future for ascii art generation

--- sprites.py
+++ sprites.py
@@ -1,26 +1,33 @@
-sprites = {"barbarian" : """
- _________________.---.______
-(_(______________(_o o_(____()
-        mrf  .___.'. .'.___.
-             \ o    Y    o /
-              \ \__   __/ /
-               '.__'-'__.'
-                   '''
-""",
+from PIL import Image
+from numpy import array
 
-           "warrior" : """
-   |\                     /)
- /\_\\__               (_//
-|   `>\-`     _._       //`)
- \ /` \\  _.-`:::`-._  //
-  `    \|`    :::    `|/
-        |     :::     |
-        |.....:::.....|
-        |:::::::::::::|
-        |     :::     |
-        \     :::     /
-         \    :::    /
-          `-. ::: .-'
-           //`:::`\\
-          //   '   \\
-         |/         \\"""}
+ASCII_CHARS = "@%#*+=-:. "
+
+
+# MEGA IMPORTANT. THIS FUNCTION DOES THE ASCII FOR YOU
+# INSTRUCTIONS: GET THE SOURCE IMAGE (remove backgrounds)
+# THEN USE THE FUNCTION AS SO 
+
+# sprites = {"thing" : image_to_ascii(r"sprites\thing.png", yourresolution)}
+
+
+def image_to_ascii(image_path, new_width=80):
+    img = Image.open(image_path).convert("RGB")  # Open image in RGB mode
+    width, height = img.size
+    aspect_ratio = height / width
+    new_height = int(new_width * aspect_ratio * 0.55)  # Adjust for terminal aspect ratio
+    img = img.resize((new_width, new_height))
+
+    pixels = array(img)
+    ascii_str = ""
+
+    for row in pixels:
+        for r, g, b in row:
+            brightness = int((r + g + b) / 3)  # Get grayscale brightness
+            char = ASCII_CHARS[brightness // 32]  # Map brightness to ASCII char
+            ascii_str += f"\033[38;2;{r};{g};{b}m{char}"  # ANSI escape color
+        ascii_str += "\033[0m\n"  # Reset color at end of line
+
+    return ascii_str  # ✅ Returns the colored ASCII string
+
+sprites = {}
