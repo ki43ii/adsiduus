43450ac67341c432031d95eb96a71cf855d6f479
ki43ii <atrpwq@proton.me>
2025-03-23 10:25:55 +0300

    edited file:   requirements.txt new file:   shockedpikachu.png modified:   sprites.py

--- sprites.py
+++ sprites.py
@@ -1,26 +1,25 @@
-sprites = {"barbarian" : """
- _________________.---.______
-(_(______________(_o o_(____()
-        mrf  .___.'. .'.___.
-             \ o    Y    o /
-              \ \__   __/ /
-               '.__'-'__.'
-                   '''
-""",
+from numpy import array
+from PIL import Image
 
-           "warrior" : """
-   |\                     /)
- /\_\\__               (_//
-|   `>\-`     _._       //`)
- \ /` \\  _.-`:::`-._  //
-  `    \|`    :::    `|/
-        |     :::     |
-        |.....:::.....|
-        |:::::::::::::|
-        |     :::     |
-        \     :::     /
-         \    :::    /
-          `-. ::: .-'
-           //`:::`\\
-          //   '   \\
-         |/         \\"""}
+ASCII_CHARS = "@%#*+=-:. "
+
+def convertToAscii(image_path, new_width=80):
+    img = Image.open(image_path).convert("RGB")
+    width, height = img.size
+    aspect_ratio = height / width
+    new_height = int(new_width * aspect_ratio * 0.55)
+    img = img.resize((new_width, new_height))
+
+    pixels = array(img)
+    ascii_str = ""
+
+    for row in pixels:
+        for r, g, b in row:
+                brightness = int((int(r) + int(g) + int(b)) / 3)
+                char = ASCII_CHARS[brightness // 32]
+                ascii_str += f"\033[38;2;{r};{g};{b}m{char}"
+        ascii_str += "\033[0m\n"
+
+    return ascii_str
+
+# tested with print(convertToAscii(r'shockedpikachu.png'))
